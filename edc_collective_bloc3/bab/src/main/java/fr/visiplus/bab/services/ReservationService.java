0aeac33f49f5d77296b3ab81d025463944c08fea
katamaric <katherinehcao@gmail.com>
2026-04-15 10:04:44 +0200

    fix reservation expiration

--- edc_collective_bloc3/bab/src/main/java/fr/visiplus/bab/services/ReservationService.java
+++ edc_collective_bloc3/bab/src/main/java/fr/visiplus/bab/services/ReservationService.java
@@ -19,63 +19,63 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class ReservationService {
-		
+
 	private BookRepository bookRepository;
-	private ReservationRepository reservationRepository;	
+	private ReservationRepository reservationRepository;
 	private UserRepository userRepository;
-	
+
 	public ReservationService(
-			final BookRepository bookRepository, 
+			final BookRepository bookRepository,
 			final ReservationRepository reservationRepository,
-			final UserRepository userRepository) {		
+			final UserRepository userRepository) {
 		this.bookRepository = bookRepository;
 		this.reservationRepository = reservationRepository;
 		this.userRepository = userRepository;
 	}
-	
+
 	@Transactional
 	public ReservationDTO bookByBookIdAndUserId(final Long bookId, final Long userId) throws Exception {
-		
+
 		User user = userRepository.getReferenceById(userId);
 		Book book = bookRepository.getReferenceById(bookId);
-		
-		if(book.getStatus().equals(BookStatus.BORROWED)) {
+
+		if (book.getStatus().equals(BookStatus.BORROWED)) {
 			throw new Exception();
-		} else if(book.getStatus().equals(BookStatus.BOOKED)) {
+		} else if (book.getStatus().equals(BookStatus.BOOKED)) {
 			LocalDate resaDate = book.getReservation().getDateResa();
 			LocalDate today = LocalDate.now();
 
 			Period period = Period.between(resaDate, today);
-			if (period.getYears() == 0 && period.getMonths() == 0 && period.getDays() <= 3) {
+			if (period.getYears() == 0 && period.getMonths() == 0 && period.getDays() <= 7) {
 				throw new Exception();
 			} else {
 				reservationRepository.deleteById(book.getReservation().getId());
 			}
-		}		
-		
+		}
+
 		book.setStatus(BookStatus.BOOKED);
-		
+
 		Reservation resa = new Reservation();
 		resa.setDateResa(LocalDate.now());
 		resa.setBook(book);
 		resa.setUser(user);
-		
+
 		resa = reservationRepository.save(resa);
-		bookRepository.save(book);		
-		
+		bookRepository.save(book);
+
 		return convert(resa);
 	}
-	
+
 	private ReservationDTO convert(final Reservation reservation) {
 		UserDTO userDTO = new UserDTO(
-				reservation.getUser().getId(), 
+				reservation.getUser().getId(),
 				reservation.getUser().getUsername());
 		BookDTO bookDTO = new BookDTO(
 				reservation.getBook().getId(),
 				reservation.getBook().getName(),
 				reservation.getBook().getDescription(),
 				reservation.getBook().getStatus());
-		ReservationDTO resaDTO = new ReservationDTO(reservation.getId(), reservation.getDateResa(), userDTO, bookDTO);		
+		ReservationDTO resaDTO = new ReservationDTO(reservation.getId(), reservation.getDateResa(), userDTO, bookDTO);
 		return resaDTO;
 	}
 
